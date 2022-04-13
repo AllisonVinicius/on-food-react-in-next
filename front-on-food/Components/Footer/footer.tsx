159585d58d8427e54499f515ab9aed8d155fcfdf
allisonvinicius <allisonvinicius63@gmail.com>
2022-04-13 18:36:05 -0400

    add styles module in footer

--- front-on-food/Components/Footer/footer.tsx
+++ front-on-food/Components/Footer/footer.tsx
@@ -1,7 +1,11 @@
+import Styles from "./footer.module.css";
+
 export function Footer() {
   return (
     <footer>
-      <p>Imersao FornExport</p>
+      <div className={Styles.footer}>
+        <p>@Allison V.Araújo estou em 40 minuo na aula</p>
+      </div>
     </footer>
   );
 }
