0a613e4a4a1abb2d883d1cf62f5cfba8644b0aaf
allisonvinicius <allisonvinicius63@gmail.com>
2022-04-19 00:11:20 -0400

    defined type return in file

--- front-on-food/Components/Dish-list/dishe-list.tsx
+++ front-on-food/Components/Dish-list/dishe-list.tsx
@@ -1,21 +1,22 @@
 import { Dishe } from "@/Components";
 import { api } from "@/services";
+import { DishType } from "@/types";
 import { useQuery } from "react-query";
 import Style from "./dish-list.module.css";
 
 export function DisheList() {
   const fetchDishs = async () => {
-    const response = await api.get("/deliveries?city=sao-paulo-sp");
+    const response = await api.get<DishType[]>("/deliveries?city=sao-paulo-sp");
     return response.data;
   };
 
-  const { data, isFetching } = useQuery("dishs", fetchDishs);
+  const { data, isFetching } = useQuery<DishType[]>("dishs", fetchDishs);
   return (
     <div className={Style.list}>
       {isFetching ? (
         <div>Carregando...</div>
       ) : (
-        data?.map((dish, index) => <Dishe key={index} />)
+        data?.map((dishe) => <Dishe key={dishe.id}{...dish} />)
       )}
     </div>
   );
