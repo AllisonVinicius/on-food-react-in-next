5f3a0693b2e3c5ebe6bd03ec72aa30d3c3468e02
allisonvinicius <allisonvinicius63@gmail.com>
2022-04-18 23:31:31 -0400

    add token in api

--- front-on-food/core/services/api.ts
+++ front-on-food/core/services/api.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://api.imersaofx.danieldcs.com/",
   headers: {
-    Autorization: "Bearer AAA",
+    Autorization: "Bearer [YWxsaXNvbnZpbmljaXVzNjNAZ21haWwuY29t]",
   },
 });
