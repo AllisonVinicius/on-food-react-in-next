304d94443c88bfc413e11311b2b97b3562e279ed
allisonvinicius <allisonvinicius63@gmail.com>
2022-04-18 23:55:39 -0400

    include fetch in api

--- front-on-food/Components/Dish-list/dishe-list.tsx
+++ front-on-food/Components/Dish-list/dishe-list.tsx
@@ -1,12 +1,22 @@
 import { Dishe } from "@/Components";
+import { api } from "@/services";
+import { useQuery } from "react-query";
 import Style from "./dish-list.module.css";
 
 export function DisheList() {
+  const fetchDishs = async () => {
+    const response = await api.get("/deliveries?city=sao-paulo-sp");
+    return response.data;
+  };
+
+  const { data, isFetching } = useQuery("dishs", fetchDishs);
   return (
     <div className={Style.list}>
-      <Dishe />
-      <Dishe />
-      <Dishe />
+      {isFetching ? (
+        <div>Carregando...</div>
+      ) : (
+        data?.map((dish, index) => <Dishe key={index} />)
+      )}
     </div>
   );
 }
